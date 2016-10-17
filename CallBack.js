e79211f50f0dd42b31a038d7cbbd6861e6bb498a
Sivanantham Padikkasu <pksivanantham@gmail.com>
2016-10-18 00:15:47 +0530

    event emitter functions added

--- CallBack.js
+++ CallBack.js
@@ -1,2 +1,15 @@
-var request=require("request");
-console.log(request)
+var eventEmitter=require("events").EventEmitter;
+console.log(eventEmitter)
+var getResource=function(c)
+{
+    var e=new eventEmitter();
+    process.nextTick(function(){
+        var count=0;
+        e.emit('start')
+    })
+    return e;
+}
+var r=getResource(1);
+r.on('start',function(){
+console.log("Event is started");
+});
