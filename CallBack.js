2413dd7a19d8ce9422d1386a6eff20fb08059ced
Sivanantham Padikkasu <pksivanantham@gmail.com>
2016-10-18 08:26:40 +0530

    Event emitter via in build events module

--- CallBack.js
+++ CallBack.js
@@ -1,15 +1,32 @@
 var eventEmitter=require("events").EventEmitter;
-console.log(eventEmitter)
+
 var getResource=function(c)
 {
     var e=new eventEmitter();
     process.nextTick(function(){
         var count=0;
         e.emit('start')
+       var runInterval= setInterval(function(){
+count++;
+e.emit('run',count);
+if(count==c)
+{
+    e.emit('stop',count);
+    clearInterval(runInterval);
+}
+
+        },100)
     })
     return e;
 }
-var r=getResource(1);
+var r=getResource(5);
 r.on('start',function(){
 console.log("Event is started");
+});
+
+r.on('run',function(count){
+console.log("Event is running :",count);
+});
+r.on('stop',function(count){
+console.log("Event is stopped the final count is  :",count);
 });
