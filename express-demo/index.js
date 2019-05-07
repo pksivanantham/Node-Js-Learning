b415cf48a56df97f2f1c5531be6d9fcd59a0a167
Sivanantham Padikkasu <pksivanantham@gmail.com>
2019-05-08 00:17:38 +0530

    Added new validation package

--- express-demo/index.js
+++ express-demo/index.js
@@ -1,12 +1,41 @@
 const express = require('express');
 const app = express();
+const Joi=require('@hapi/joi');
+app.use(express.json());
 
+const courses = [
+    {
+        id: 1,
+        name: 'Tamil'
+    },
+    {
+        id: 2,
+        name: 'English'
+    }
+];
 app.get('/', (req, res) => {
 
-    res.send('Hello Express');
+    res.send('Hello Express Js');
 });
 
 app.get('/api/courses', (req, res) => {
-    res.send([1, 2, 3]);
+    res.send(courses);
 });
-app.listen(5000,()=>{ console.log('Listening on port 5000..')});
+
+app.get('/api/courses/:id', (req, res) => {
+    let course= courses.find((item)=>item.id==req.params.id);
+    if(!course) res.status(404).send("Resource not found");        
+    res.send(course);
+});
+
+app.post('/api/courses', (req, res) => {
+    const course = { 
+        id: courses.length + 1, 
+        name: req.body.name 
+    };
+    courses.push(course);        
+    res.send(course);
+});
+
+const port = process.env.TESTPORT||5000; //POWERSHELL COMMAND :$env:<Variable_name> = value
+app.listen(port,()=>{ console.log(`Listening on port ${port}..`)});
