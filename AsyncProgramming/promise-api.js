9a5a36cb2ac751d78f49a7173050589925cebe18
Sivanantham Padikkasu <pksivanantham@gmail.com>
2019-07-23 23:23:03 +0530

    promise api functions added

--- AsyncProgramming/promise-api.js
+++ AsyncProgramming/promise-api.js
@@ -4,3 +4,34 @@ const p = Promise.resolve('I am already resolved');
 p
 .then((data)=>console.log(data))
 .catch((err)=>console.log('Error',err));
+
+var p1 = new Promise((resolve,reject)=>{
+
+    setTimeout(() => {
+        
+        resolve('From promise 1');
+
+    }, 3000);
+
+});
+
+var p2 = new Promise((resolve,reject)=>{
+
+    setTimeout(() => {
+        
+        resolve('From promise 2');
+        //reject(new Error('Promise 2 ran into error'));
+        
+    }, 3000);
+
+});
+
+Promise
+.all([p1,p2])
+.then((d)=>console.log('all',d))
+.catch((err)=>console.log(err));
+
+Promise
+.race([p1,p2])
+.then((d)=>console.log('race',d))
+.catch((err)=>console.log(err));
