eafd28d846d933342181c67ea09447dce3c2faef
Sivanantham Padikkasu <pksivanantham@gmail.com>
2019-08-17 13:42:09 +0530

    Updated the code to handle array of sub docs

--- mongo-demo/Embedding Documents/embedding.js
+++ mongo-demo/Embedding Documents/embedding.js
@@ -14,16 +14,16 @@ const Author = mongoose.model('Author', authorSchema);
 
 const Course = mongoose.model('Course', new mongoose.Schema({
   name: String,
-  author:{//Embedded Docouments here
-    type:authorSchema,
+  authors:{//Embedded Docouments here
+    type:[authorSchema],
     required:true
   }
 }));
 
-async function createCourse(name, author) {
+async function createCourse(name, authors) {
   const course = new Course({
     name, 
-    author
+    authors
   }); 
   
   const result = await course.save();
@@ -32,26 +32,72 @@ async function createCourse(name, author) {
 
 async function listCourses() { 
   const courses = await Course.find();
+  console.log(courses[0].authors[0].name)
   console.log(courses);
 }
 
-async function updateCourse(courseId)
+async function updateCourse(courseId,authorId)
 {
-   await Course.updateOne({_id:courseId},{
-    $set:{
-      'author.name':'Siva'
-    }
-  })
+  //  await Course.updateOne({_id:courseId},{
+  //   $set:{
+  //     'author.name':'Siva'
+  //   }
+  // })
+  let course = await Course.findById({_id:courseId});
+
+  let authors = course.authors.id(authorId);
+
+  authors.name= `${authors.name} -->Updated`;
+
+  /*
+  mongoose: 
+  calling `save()` on a subdoc does **not** save the document to MongoDB, it only runs save middleware.
+   Use `subdoc.save({ suppressWarning: true })` to hide this warning if you're sure this behavior is right for your app. 
+  */
+  await authors.save({suppressWarning:true});
+
+  console.log(authors);
+
 }
-async function deleteAuthorInsideCourse(courseId)
+
+async function addNewAuthor(courseId,author)
 {
-   await Course.updateOne({_id:courseId},{
-    $unset:{
-      'author':''
-    }
-  })
+
+  let course = await Course.findById({_id:courseId});
+
+  course.authors.push(author);
+
+  await course.save();
+
+  console.log(course);
 }
-//createCourse('Node Course', new Author({ name: 'Sivanantham' }));
+async function deleteAuthorInsideCourse(courseId,authorId)
+{
+  //  await Course.updateOne({_id:courseId},{
+  //   $unset:{
+  //     'author':''
+  //   }
+  // })
+
+  let course = await Course.findById({_id:courseId});
+
+  let author = course.authors.id(authorId);
+
+  course.authors.remove(author);
+
+  await course.save();
+
+  console.log(course);
+}
+
+
+// createCourse('Node Course',[
+//   new Author({ name: 'Groot' }),
+//   new Author({ name: 'Rocket' })
+// ]);
 //listCourses();
-//updateCourse('5d579d5f6ca3b907307a581d');
-deleteAuthorInsideCourse('5d579d5f6ca3b907307a581d');
+//updateCourse('5d57aee8a60e2929b85395ca','5d57aee8a60e2929b85395c8');
+//addNewAuthor('5d57aee8a60e2929b85395ca',new Author({name:'StarLord'}));
+deleteAuthorInsideCourse('5d57aee8a60e2929b85395ca','5d57aee8a60e2929b85395c8');
+
+//deleteAuthorInsideCourse('5d579d5f6ca3b907307a581d');
