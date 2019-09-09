2331855d78d6aaf1fb9321d8c4e673ddd73214c4
Sivanantham Padikkasu <pksivanantham@gmail.com>
2019-09-09 13:08:41 +0530

    new package 'express-async-errors' added

--- vidly/middleware/async.js
+++ vidly/middleware/async.js
@@ -1,3 +1,6 @@
+//This is replaced with the npm lib 'express-async-errors'
+//If the above package didn't work then we can use the below helper-
+//function to wrap the handlers in routes
 module.exports = function(handler) {
     return async (req, res, next) => {
   
