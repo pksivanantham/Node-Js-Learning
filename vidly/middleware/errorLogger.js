c3d0dadc3f6af2d874236811047c97d0ecf94a55
Sivanantham Padikkasu <pksivanantham@gmail.com>
2019-09-27 00:35:53 +0530

    logged the error into mongodb

--- vidly/middleware/errorLogger.js
+++ vidly/middleware/errorLogger.js
@@ -1,17 +1,23 @@
 const winston = require('winston');
+const { format : {combine, timestamp, prettyPrint }} = winston;
+require("winston-mongodb")
 const logger = winston.createLogger({
     level: 'error',
-    format: winston.format.json(),
+    format: combine(winston.format.json(),
+    timestamp(),
+    prettyPrint()),
     transports: [
         new winston.transports.File({ filename: 'logfile.log', level: 'error' })
     ]
 });
 
 logger.add(new winston.transports.Console({
-    format: winston.format.combine(
+    format: combine(
         winston.format.colorize({ all: true }),
         winston.format.simple()
     )
 }));
+
+logger.add(new winston.transports.MongoDB({db:'mongodb://localhost/vidly'}))
 
 module.exports = logger;
