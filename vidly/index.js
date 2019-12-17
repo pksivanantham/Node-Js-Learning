70425bad58782bbe6e4d73a06121b5b7a53811e7
Sivanantham Padikkasu <pksivanantham@gmail.com>
2019-12-17 22:52:28 +0530

    refactored routes

--- vidly/index.js
+++ vidly/index.js
@@ -1,19 +1,13 @@
 require('express-async-errors');
 const winston = require('winston');
 const logger = require('./middleware/errorLogger');
-const error = require('./middleware/error');
 const Joi = require('@hapi/joi');
-const genres = require('./routes/genres');
-const customers = require('./routes/customers');
-const movies = require('./routes/movies');
-const rentals = require('./routes/rentals');
-const users = require('./routes/users');
-const auth = require('./routes/auth');
 const config = require('config');
 const mongoose = require('mongoose');
 const morgan = require('morgan');
 const express = require('express');
 const app = express();
+require('./startup/routes')(app);
 Joi.objectId = require('joi-objectid')(Joi);
 
 if (!config.has('jwtPrivateKey')|| (config.get('jwtPrivateKey')==="")) {
@@ -42,16 +36,7 @@ mongoose.connect('mongodb://localhost/vidly', { useNewUrlParser: true })
   .catch((err) => console.log('Unable to connect to the database..', err));
 mongoose.set('useCreateIndex', true);//To remove unique index warning on mongoose
 
-app.use(express.json());
 app.use(morgan('dev'));//Added for logging network requests
-
-app.use('/api/genres', genres);
-app.use('/api/customers',customers);
-app.use('/api/movies',movies);
-app.use('/api/rentals',rentals);
-app.use('/api/users',users);
-app.use('/api/auth',auth);
-app.use(error);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
