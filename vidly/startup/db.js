b50f329afeb5e83e1f512cce3b4bf30112640505
Sivanantham Padikkasu <pksivanantham@gmail.com>
2019-12-18 01:04:04 +0530

    log method updated

--- vidly/startup/db.js
+++ vidly/startup/db.js
@@ -3,7 +3,7 @@ const winston = require('winston');
 
 module.exports = function () {
     mongoose.connect('mongodb://localhost/vidly', { useNewUrlParser: true })
-        .then(() => winston.log('Connected to the database..'))
+        .then(() => winston.log('info','Connected to the database..'))
     mongoose.set('useCreateIndex', true);//To remove unique index warning on mongoose
 
 }
