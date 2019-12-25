d8810a9d833170d0b52c9b58cde2afcd51af4fff
Sivanantham Padikkasu <pksivanantham@gmail.com>
2019-12-25 15:56:09 +0530

    new tests added for test the absolute function in lib.js

--- testing-demo/tests/lib.test.js
+++ testing-demo/tests/lib.test.js
@@ -1,7 +1,20 @@
-test('My sample unit test with jest',()=>{
+const lib = require('../lib')
+
+test('Absolute-Result should return positive number if input is positive',()=>{
+
+    let result = lib.absolute(2);
+    expect(result).toBe(2);
 
 });
-test('My sample unit test with jest:throwing error',()=>{
 
-    throw new Error('Hey i am failed');
+test('Absolute-Result should return positive number if input is negative',()=>{
+
+    let result = lib.absolute(-2);
+    expect(result).toBe(2);
+
+});
+
+test('Absolute-Result should return 0 if input is 0',()=>{
+    let result = lib.absolute(0);
+    expect(result).toBe(0);
 });
