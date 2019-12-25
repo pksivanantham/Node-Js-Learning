f6243b39191ce08507a3e04066d38aca5650b22a
Sivanantham Padikkasu <pksivanantham@gmail.com>
2019-12-25 17:34:32 +0530

    refactored the lib fn and and test file

--- testing-demo/tests/lib.test.js
+++ testing-demo/tests/lib.test.js
@@ -1,20 +1,24 @@
 const lib = require('../lib')
 
-test('Absolute-Result should return positive number if input is positive',()=>{
+describe('absolute', () => {
 
-    let result = lib.absolute(2);
-    expect(result).toBe(2);
+    it('should return positive number if input is positive', () => {
 
-});
+        let result = lib.absolute(2);
+        expect(result).toBe(2);
 
-test('Absolute-Result should return positive number if input is negative',()=>{
+    });
 
-    let result = lib.absolute(-2);
-    expect(result).toBe(2);
+    it('should return positive number if input is negative', () => {
 
-});
+        let result = lib.absolute(-2);
+        expect(result).toBe(2);
 
-test('Absolute-Result should return 0 if input is 0',()=>{
-    let result = lib.absolute(0);
-    expect(result).toBe(0);
-});
+    });
+
+    it('should return 0 if input is 0', () => {
+        let result = lib.absolute(0);
+        expect(result).toBe(0);
+    });
+})
+
