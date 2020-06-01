5f591bffdb2f83e82771884297401c852b52be33
Sivanantham Padikkasu <pksivanantham@gmail.com>
2020-06-01 14:49:13 +0530

    Added tests for 'PUT' & 'DELETE ' API methods

--- vidly/routes/genres.js
+++ vidly/routes/genres.js
@@ -26,7 +26,7 @@ router.post('/', auth, async (req, res) => {
   res.send(genre);
 });
 
-router.put('/:id', auth, async (req, res) => {
+router.put('/:id', [auth,validateObjectId], async (req, res) => {
   const { error } = validate(req.body);
   if (error) return res.status(400).send(error.details[0].message);
 
@@ -42,7 +42,7 @@ router.put('/:id', auth, async (req, res) => {
   res.send(genre);
 });
 
-router.delete('/:id', [auth, admin], async (req, res) => {
+router.delete('/:id', [auth, admin,validateObjectId], async (req, res) => {
   const genre = await Genre.findByIdAndDelete({ _id: req.params.id });
   if (!genre) return res.status(404).send('The genre with the given ID was not found.');
   res.send(genre);
--- vidly/tests/integration/genres.test.js
+++ vidly/tests/integration/genres.test.js
@@ -11,7 +11,7 @@ describe('/api/genres', () => {
     afterEach(async () => {
         server.close();
         await Genre.collection.deleteMany({});
-        
+
     })
     describe('GET /', () => {
         it('should return all the genres', async () => {
@@ -40,7 +40,7 @@ describe('/api/genres', () => {
             expect(req.body).toHaveProperty('name', genre.name);
         });
         it('should return the 404 status if genre is not found for the given id', async () => {
-        
+
             let id = mongoose.Types.ObjectId();
             let req = await request(server).get(`/api/genres/${id}`);
             expect(req.status).toBe(404);
@@ -62,7 +62,7 @@ describe('/api/genres', () => {
             name = 'Thriller';
         })
 
-        callServer = async () => {
+        let callServer = async () => {
             return await request(server)
                 .post(`/api/genres`)
                 .set('x-vidly-jwt', token)
@@ -110,5 +110,157 @@ describe('/api/genres', () => {
 
         })
 
-    })
+    });
+    describe('PUT ', () => {
+      let token;
+        let name;
+        let _id;
+
+
+        beforeEach(() => {
+            token = new User().generateToken();
+            name = 'Thriller';
+        })
+
+     let postGenre = async () => {
+         
+            return await request(server)
+                .put(`/api/genres/${_id}`)
+                .set('x-vidly-jwt', token)
+                .send({ name });
+
+        };
+        let createGenre = async ()=>{
+
+            return await request(server)
+                .post(`/api/genres`)
+                .set('x-vidly-jwt', token)
+                .send({ name: 'Genre1' });
+        };
+
+        it('should update genre if valid input passed', async () => {
+
+            let res = await createGenre();
+
+            expect(res.status).toBe(200);
+
+            _id = res.body._id;
+            res = await postGenre();
+
+            expect(res.status).toBe(200);
+            expect(res.body).toHaveProperty('name', name);
+
+
+        });
+
+        it('should return 400 status if invalid input passed', async () => {
+
+            _id = mongoose.Types.ObjectId().toHexString();
+            name ='12';
+            res = await postGenre();
+            expect(res.status).toBe(400);
+
+
+        });
+
+        it('should return 404 status if invalid genre id is passed', async () => {
+
+            
+            _id = mongoose.Types.ObjectId().toHexString();
+
+            res = await postGenre();
+
+            expect(res.status).toBe(404);
+
+
+        });
+
+
+    });
+
+    describe('DELETE ', () => {
+        let token;
+          let _id;
+  
+  
+          beforeEach(() => {
+            const user = {
+                _id :mongoose.Types.ObjectId().toHexString(),
+                isAdmin:true
+            }
+    
+             token = new User(user).generateToken();
+    
+          })
+  
+        let deleteGenre = async () => {
+  
+        
+              return await request(server)
+                  .delete(`/api/genres/${_id}`)
+                  .set('x-vidly-jwt', token);
+  
+          };
+          let createGenre = async ()=>{
+  
+              return await request(server)
+                  .post(`/api/genres`)
+                  .set('x-vidly-jwt', token)
+                  .send({ name: 'Genre1' });
+          };
+  
+          it('should delete genre if valid genre id passed', async () => {
+  
+              let res = await createGenre();
+  
+              expect(res.status).toBe(200);
+  
+              _id = res.body._id;
+
+              res = await deleteGenre();
+  
+              expect(res.status).toBe(200);
+
+             let genre = await Genre.findById({_id})
+
+             expect(genre).toBeNull();
+  
+          });
+  
+          it('should return 403 status if the user is not admin', async () => {
+  
+            token = new User().generateToken();
+
+            res = await deleteGenre();
+            expect(res.status).toBe(403);
+//            console.log(res.text)
+
+
+        });
+
+          it('should return 404 status if invalid input passed', async () => {
+  
+              _id = 'ID';
+
+              res = await deleteGenre();
+              expect(res.status).toBe(404);
+  
+  
+          });
+  
+          it('should return 404 status if invalid genre id is passed', async () => {
+  
+              
+              _id = mongoose.Types.ObjectId().toHexString();
+  
+              res = await deleteGenre();
+  
+              expect(res.status).toBe(404);
+  
+  
+          });
+  
+  
+      });
+
 });
